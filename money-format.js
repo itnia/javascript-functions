136744fd06bf9d376a5c79844c1f53accb448f63
itnia <itnia.aurey@outlook.com>
2019-02-28 20:47:38 +0300

    commit

--- money-format.js
+++ money-format.js
@@ -16,6 +16,46 @@ var Money = (function () {
             console.log(e)
         }
     };
-// return element
     return element;
 }());
+
+//////////////////////////////////////////////////////////////////////////////
+function formatNumInt (amount){
+    let str = '', k = (amount < 0 ? '' + amount : '+' + amount);
+
+    for(var i = k.length; i > 4; i -= 3){
+      str = ',' + k.slice(i - 3, i) + str;
+    }
+    return (k[0] == '+' ? '' : '-') + k.slice(1, i) + str;
+}
+
+function formatNum (amount, decimalCount = 2, decimal = ".", thousands = ","){
+    let str = '', k = (amount < 0 ? '' + amount.toFixed(decimalCount) : '+' + amount.toFixed(decimalCount));
+
+    for(var i = k.length - (decimalCount + 1); i > 4; i -= 3){
+      str = thousands + k.slice(i - 3, i) + str;
+    }
+
+    return (k[0] == '+' ? '' : '-') + k.slice(1, i) + str + decimal + k.slice(k.length - decimalCount);
+}
+//////////////////////////////////////////////////////////////////////////////
+function formatInt(amount){
+
+    for(var n = amount, x = ''; (n > 999) || (n < -999); n = ~~(n / 1000)){
+        x = "'" + ("" + n).slice(-3) + x;
+    }
+    
+    return n + x;
+}
+
+function formatFloat(amount, decimalCount = 2, decimal = ".", thousands = ","){
+
+    var n = (Math.round(amount * 10**decimalCount));
+    var x = decimalCount ? (decimal + ("" + n).slice(-decimalCount)) : '';
+
+    for(n = ~~(n / 10**decimalCount); (n > 999) || (n < -999); n = ~~(n / 1000)){
+        x = thousands + ("" + n).slice(-3) + x;
+    }
+
+    return n + x;
+}
